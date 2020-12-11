229245c8afb765a2da5892003a7114a39610e4b4
Alan <medelalan23@gmail.com>
2020-12-10 17:41:11 -0800

    Deleted unneccesarry items

--- IT-222-master/README.md
+++
@@ -1,3 +0,0 @@
-IT - 222
-======
-Advanced Javascript Course at Seattle Central College
--- IT-222-master/week-3/really-large-application/index.js
+++
@@ -1,7 +0,0 @@
-const colors = require('colors');
-
-const chosenColor = colors.getRandomColor();
-console.log(`You should use ${chosenColor.name} on your website. It's HTML code is ${chosenColor.code}`);
-
-const favoriteColor = colors.getBlue();
-console.log(`My favorite color is ${favoriteColor.name}/${favoriteColor.code}, btw`);
